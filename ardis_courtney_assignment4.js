27dd7e7b7a830f432cb58af0fa5a9cbef2371ee8
Courtney Ardis <CourtneyArdis@Courtneys-MacBook-Pro.local>
2012-04-21 21:31:52 -0400

    Added stub functions

--- ardis_courtney_assignment4.js
+++ ardis_courtney_assignment4.js
@@ -1,1 +1,62 @@
-alert("JavaScript works!");
+//Strings
+var isPhoneNum = function (phoneNum) {
+	//TODO: Fill in working code here that tests whether a string follows a phone number pattern such as 123-456-7890
+	return {};
+}
+
+var isEmail = function (emailAddress) {
+	//TODO: Fill in working code here that test s whether a string follows an email address pattern such as aaa@bbb.ccc
+	return {};
+}
+
+var isURL = function (url) {
+	//TODO: Fill in working code here that tests whether a string is a URL. Does it start with http: or https:?
+	return {};
+}
+
+var makeTitle = function () {
+	//TODO: Fill in working code here that takes a string, splits into words, then uppercases the first letter of each word
+	return {};
+}
+
+var changeSeparator = function () {
+	//TODO: Fill in working code here that given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" --> "a/b/c".
+	return {};
+}
+
+//Numbers
+var formatNum = function (number) {
+	//TODO: Fill in working code here that formats a number to use a specific number of decimal places such as money: 2.1 --> 2.10
+	return {};
+}
+
+var matchNum = function (number) {
+	//TODO: Fill in working code that fuzzy-matches a number: is the number above or below a number within a certain percent?
+	return {};
+}
+
+var findDays = function (dates) {
+	//TODO: Fill in working code that finds the number of days difference between two dates.
+	return {};
+}
+
+var convertString = function (string) {
+	//TODO: Fill in working code that given a string version of a number such as "42", return the value as an actual Number, such as 42.
+	return {};
+}
+
+//Arrays
+var findNum = function (number) {
+	//TODO: Fill in working code that finds the smallest value in an array that is greater than a give number.
+	return {};
+}
+
+var findValue = function (numbers) {
+	//TODO: Fill in working code that finds the total value of just the numbers in an array, even if some of the items are not numbers.
+	return {};
+}
+
+var sort = function (array) {
+	//TODO: Fill in working code that given an array of objects and the name of a key, rturn the array sorted by the value of that key in each of the objects: "a" + [{a:2}, {a:3}, {a:1}] --> [{a:1}, {a:2}, {a:3}]
+	return {};
+}
