de93cedf1240cb3bba251d40b24c2240d1aea96a
ardiscb <ardis.courtney@gmail.com>
2012-04-25 13:08:33 -0300

    Make Up While loop and added some code for a few functions. Need to commit from computer terminal to add library to github.

--- ardis_courtney_assignment4.js
+++ ardis_courtney_assignment4.js
@@ -1,5 +1,4 @@
 
-
 //Strings
 var isPhoneNum = function (phoneNum) {
 	//TODO: Fill in working code here that tests whether a string follows a phone number pattern such as 123-456-7890
@@ -8,7 +7,7 @@ var isPhoneNum = function (phoneNum) {
 
 //Function: tests whether a string follows an email address pattern such as aaa@bbb.ccc
 //Not outputting correctly when going through if statement to see if there is a "." in the email address
-var isEmail = function () {
+var isEmail = function (emailAdd) {
 	email = "ardiscb@fullsail.edu"
 	if (email.match("@") && email.match(".")) {
 		console.log("This is an email address.");
@@ -16,7 +15,9 @@ var isEmail = function () {
 	else {
 		console.log("This is not an email address");
 	};
-	return {};
+	return {
+		"email": isEmail
+	};
 };
 
 isEmail();
@@ -43,14 +44,25 @@ var makeTitle = function (words) {
 var changeSeparator = function (string, string2) {
 	//TODO: Fill in working code here that given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: 
 	//"a,b,c" + "," + "/" --> "a/b/c".
+	/* 
+	//Fill in for statement
+	for () {
+		".".replace(", ");
+	}
+	*/
 	return {};
 }
+changeSeparator("laptop.phone.bag.keys");
 
-//Numbers
+//Numbers (not working)
 var formatNum = function (number) {
 	//TODO: Fill in working code here that formats a number to use a specific number of decimal places such as money: 2.1 --> 2.10
-	return {};
+	number = Math.floor();	
+	return {
+		"number": formatNum
+	};
 }
+formatNum(2.15);
 
 var matchNum = function (number) {
 	//TODO: Fill in working code that fuzzy-matches a number: is the number above or below a number within a certain percent?
@@ -80,27 +92,41 @@ var findValue = function (numbers) {
 
 var sort = function (array) {
 	//TODO: Fill in working code that given an array of objects and the name of a key, rturn the array sorted by the value of that key in each of the objects: "a" + [{a:2}, {a:3}, {a:1}] --> [{a:1}, {a:2}, {a:3}]
-	return {};
+	array = [];
+	return {
+		"array": sort
+	};
 }
 
 
 /* 
 
 //Make Up: String Variable, Deliverable 2
-var name = "Courtney";
+	var name = "Courtney";
 
-//Make Up: While Loop, Deliverable 2
-while () {
+//Make Up: While Loop, Deliverable 2 and 3 (working)
+	var i = 0;
+	while (i < 5) {
+		console.log("i is " + i);
+		i++;
+	};
 
-};
-
-//Make Up: Scope, Deliverable 2
+//Make Up: Argument Array, Deliverable 3
 
 
-//Make Up: Arguments, Deliverable 2
+//Make Up: Argument Boolean, Deliverable 3
 
 
-//Make Up: Returns, Deliverable 2
+//Make Up: Argument Number, Deliverable 3
+
+
+//Make Up: Return Array, Deliverable 3
+
+
+//Make Up: Return Number, Deliverable 3
+
+
+//Make Up: Return Object, Deliverable 3
 
 
 */
